e6b55bdf74628c88797b22fc9096c3664326725f
k-kanada <kanada@pcl.cs.waseda.ac.jp>
2022-03-06 21:29:25 +0900

    refactor: replace poetry with pdm

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
 
 git clone https://github.com/kanada0727/project-templates ~/.generate-templates --depth 1
-rm -rf ~/.generate-templates/.git
 echo "export PATH=\"\$PATH:~/.generate-templates/\"" >> ~/.bashrc
